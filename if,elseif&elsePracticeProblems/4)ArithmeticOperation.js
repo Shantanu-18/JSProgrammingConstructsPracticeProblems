eaa1a38393b2c3b2cdcb022dc0ac6cccb7ef7b51
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-21 23:26:36 +0530

    [Shantanu] Added : Finding min and max of four arithmetic operations.

--- if,elseif&elsePracticeProblems/4)ArithmeticOperation.js
+++ if,elseif&elsePracticeProblems/4)ArithmeticOperation.js
@@ -12,5 +12,5 @@ let y = a % b + c;
 let z = c + a / b;
 let w = a * b + c;
 
-console.log("Maximum value = "+Math.max(x,y,z,w));
-console.log("Minimum value = "+Math.min(x,y,z,w));
+console.log("Maximum value = " + Math.max(x, y, z, w));
+console.log("Minimum value = " + Math.min(x, y, z, w));
